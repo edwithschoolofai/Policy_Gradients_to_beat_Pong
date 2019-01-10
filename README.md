c516a96c466adc236fefc6e04d9530f6479b8018
gpko369 <44971629+gpko369@users.noreply.github.com>
2019-01-11 03:42:01 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# Policy_Gradients_to_beat_Pong
-This is the code for the "How to Beat Pong Using Policy Gradients (LIVE)" by Siraj Raval on Youtube
+# 정책 경사법으로 퐁 정복하기
+Siraj Raval 의 유투브 영상인 "How to Beat Pong Using Policy Gradients (LIVE)" 에 대한 코드입니다.
 
-## Overview
+## 개요
 
-This is the code for [this](https://www.youtube.com/watch?v=PDbXPBwOavc) video by Siraj Raval on Youtube. We're going to beat the game of Pong using Policy Gradients (a type of reinforcement algo). PG outperformed DeepMind's Deep Q Network, so its a worthy algo to look into. 
+Siraj Raval 의 유투브 [영상](https://www.youtube.com/watch?v=PDbXPBwOavc)에 대한 코드입니다. 정책 경사법 (강화된 알고리즘 중 하나)를 이용해 퐁을 정복해볼 것입니다. 딥마인드의 딥Q네트워크보다 뛰어난 성능을 보여주기 때문에 살펴볼 가치가 있습니다. 
 
-## Dependencies
+## 종속성
 
 * gym (https://gym.openai.com/docs)
 * numpy 
 * pickle
 
-Install dependencies with [pip](https://pip.pypa.io/en/stable/installing/)
+[pip](https://pip.pypa.io/en/stable/installing/) 를 사용해 설치
 
-## Usage
+## 사용법
 
-Run `demo.py` and the AI will start playing the game
+`demo.py` 를 실행하면 AI 가 게임을 시작합니다
 
-## Credits
+## 참조
 
-Credits go to [AndrejK](https://github.com/karpathy) i've merely created a wrapper to get people started.
+[AndrejK](https://github.com/karpathy) 의 코드를 참조하여 간단하게 구성한 것입니다.
--- demo.py
+++ demo.py
@@ -1,222 +1,219 @@
-""" Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym. """
-#matrix math
+""" 퐁에서 정책 경사법을 이용해 에이전트를 훈련하며, OpenAI Gym 을 사용합니다. """
+#행렬
 import numpy as np 
-#for loading and saving models. Pickling is the process whereby a Python object hierarchy is converted into a byte stream)
+#모델을 저장하고 불러오기 위함. 피클링은 파이썬 객체의 계층을 바이트 스트림으로 변환하는 과정입니다.
 import cPickle as pickle 
-#OpenAI's library that provides environments to test RL algorithms in, Universe adds
-#even more environments
+#RL 알고리즘을 테스트할 환경을 제공하는 OpenAI 라이브러리
 import gym
 
-#Step 1 - How does pong work?
-#1 - receive image frame
-#2 - move paddle UP/DOWN? (binary)
-#3 - Make action recieve reward (+1 if moves past AI, -1 if missed ball, 0 otherwise)
-#lets forget about other pong details. focus on the algo.
+#1 단계 - 퐁은 어떻게 작동할까요?
+#1 - 이미지 프레임을 받습니다
+#2 - 패들을 위/아래로 움직입니다
+#3 - 동작을 통해 보상을 받습니다 (AI 를 제치면 +1, 공을 놓치면 -1, 그 외는 0)
+#다른 세부사항들은 넘기고, 알고리즘에 집중해봅시다
 
-#Step 2 - On RL
-#1 RL is a Branch of machine learning concerned with taking #sequences of actions
-#Usually described in terms of agent interacting with a
-#previously unknown environment, trying to maximize cumulative reward
-#2 RL combined with other techniques is powerful (AlphaGo, DQN)
-#Policy Gradients > DQN (by most ppl, incl. DQN Authors) https://www.youtube.com/watch?v=M8RfOCYIL8k
-#3 we'll build a 2 layer fully connected neural network
+#2 단계 - RL 적용
+#1 RL 은 머신러닝 중에서 연속적인 동작에 관한 분야입니다
+#주로 하나의 에이전트가 미지의 환경과의 상호작용 속에서
+#보상을 최대화하는 과정으로 이해할 수 있습니다
+#2 RL 과 다른 기술을 접목하면 굉장히 강력합니다 (알파고, DQN)
+#정책 경사법 > DQN (DQN 의 저자를 포함한 대부분의 ppl 에서 그렇습니다) https://www.youtube.com/watch?v=M8RfOCYIL8k
+#3 w두 개의 완전연결 신경망 층을 구축합니다
 #https://karpathy.github.io/assets/rl/policy.png
-#recieves image pixels, outputs probability of moving UP (stochasticity!)
+#이미지 픽셀을 받아서, 위쪽 이동의 확률을 출력합니다 (확률론!)
 
-#Step 3  Policy gradients have 3 key differences from supervised learning
-#1 We dont have the correct labels so as a fake label we substitute the action we happened to sample from the policy 
-#2 We modulate the loss for each example multiplicatively based on the eventual outcome
-#since we want to increase the log probability for actions that worked and decrease it for those that didnt. 
-#3 runs on a continuously changing dataset (the episodes), scaled by the advantage, and we only want to do one (or very few) 
-#updates based on each sampled dataset.
-#4 can be used to backprop when nodes are stochastic (!!, more on this later)
+#3 단계 - 정책 경사법은 지도 학습과 3 가지의 주된 차이점이 있습니다
+#1 올바른 라벨이 아닌 가짜 라벨이 있기 때문에 실제 행동을 정책의 샘플로 대체합니다
+#2 실제 결과에 기반하여 각 예시의 손실을 조절합니다
+#제대로 작동했을 때 행동의 확률의 로그값은 증가하고, 그렇지 않았을 때의 값은 감소해야 합니다.
+#3 변하는 데이터 세트에서 실행되고, 앞서있는 비율을 따지면서, 하나의 샘플 데이터세트 당 하나의 갱신을 해줍니다
+#4 노드가 확률론적이면 역전파를 사용할 수 있습니다 (이후에 더 알아볼 것입니다)
 
-#Step 4 - PG vs Humans
-#Policy Gradients have to actually experience a positive reward, and experience it very often in order to eventually and slowly shift the policy parameters towards 
-#repeating moves that give high rewards. With our abstract model, humans can figure out what is likely to give rewards without ever actually experiencing the 
-#rewarding or unrewarding transition. I dont have to actually experience crashing my car into a wall a few hundred times before I slowly start avoiding to do so.
+#4 단계 - PG vs 인간
+#정책 경사법은 반드시 긍정 보상을 경험해야하고, 정책 변수를 더 높은 보상을 주는 방향으로 행동을 서서히 바꾸기 위해 더 자주 경험해야 합니다
+#이 추상 모델에서는 보상 체계를 직접 경험하지 않고서도 인간은 어떤 것으로 보상을 얻는지 알 수 있습니다
+#단순히 앞으로 그러지 않기 위해 수백번 차로 벽을 박을 필요는 없는 것 같이 말입니다
 
-#Step 5 - Credit Assignment problem
-#Credit assignment problem - we just got +1 reward. what caused it? millions parameters and pixels...which frame? 
-#no labels!
+#5단계 - 책임 할당 문제
+#책임 할당 문제 - +1 을 얻었을 때. 어떤 행동이 원인이었을까요? 수 백만 개의 변수와 픽셀 중 어떤 프레임일까요? 
+#라벨은 없습니다
 #https://karpathy.github.io/assets/rl/rl.png
-#forward pass > output probability distribution > sample from it to take an action > wait for reward > use reward as gradient
-#to update network via backprop
-#reward could be any positive number. doesnt matter. its about magnitude. thanks neural nets.
+#forward pass > 출력 확률 분포 > 이곳에서 나온 동작을 위한 샘플 > 보상 대기 > 보상을 경사로 이용해 역전파를 통해 네트워크를 갱신
+#보상은 어떤 양수든 가능합니다. 신경망 덕분에 크기만 신경쓰면 됩니다
 
-#Training time - 3 nights on a macbook, couple hours on GPU cluster in AWS
+#훈련 시간 - 맥북으로 3 일, AWS 의 GPU 에서는 몇 시간
 
 
-# hyperparameters
-H = 200 # number of hidden layer neurons
-batch_size = 10 # every how many episodes to do a param update?
-learning_rate = 1e-4 #for convergence (too low- slow to converge, too high,never converge)
-gamma = 0.99 # discount factor for reward (i.e later rewards are exponentially less important)
+# 하이퍼파라미터
+H = 200 # 은닉층 뉴런의 개수
+batch_size = 10 # 변수 갱신 주기
+learning_rate = 1e-4 #수렴하도록 하기 위함 (너무 느리면- 수렴하는데 시간이 걸린다, 너무 높으면 - 수렴하지 않는다)
+gamma = 0.99 # 보상에 대한 할인률 (i.e 이후의 보상은 훨씬 덜 중요합니다)
 decay_rate = 0.99 # decay factor for RMSProp leaky sum of grad^2
-resume = False # resume from previous checkpoint?
+resume = False # 이전 체크포인트에서 시작합니다
 
-# model initialization
-D = 80 * 80 # input dimensionality: 80x80 grid (the pong world)
+# 모델 초기화
+D = 80 * 80 # 입력 크기: 80x80
 if resume:
   model = pickle.load(open('save.p', 'rb')) #load from pickled checkpoint
 else:
-  model = {} #initialize model 
-  #rand returns a sample (or samples) from the standard normal distribution
-  #xavier algo determines the scale of initialization based on the number of input and output neurons.
-  #Imagine that your weights are initially very close to 0. What happens is that the signals shrink as it goes through each 
-  #layer until it becomes too tiny to be useful. Now if your weights are too big, the signals grow at each layer 
-  #it passes through until it is too massive to be useful.
-  #By using Xavier initialization, we make sure that the weights are not too small but not too big to propagate accurately the signals.
-  model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" initialization
+  model = {} #모델을 초기화합니다
+  #표준 정규 분포로부터 샘플을 반환합니다
+  #Xavier 알고리즘이 입력과 출력 뉴런 개수에 따라 초기화 비율을 결정합니다.
+  #가중치가 0 에 매우 가깝다고 상상해본다면, 각 층을 지날 때마다 신호가 줄어들어 사용하기에 너무 작게 됩니다.
+  #만약 가중치가 너무 크다면, 각 층을 지날 때마다
+  #신호의 크기가 증가해 사용하기에 너무 커집니다.
+  #Xavier 초기화를 이용함으로써, 가중치가 너무 작거나 크지 않게 만들어 신호를 정확하게 전달할 수 있게 됩니다
+  model['W1'] = np.random.randn(H,D) / np.sqrt(D) # "Xavier" 초기화
   model['W2'] = np.random.randn(H) / np.sqrt(H)
-  #zeros like returns an array of zeros with the same shape and type as a given array.
-  #we will update buffers that add up gradients over a batch
-  #where the model contains kv pairs, weights layers etc 
+  #zeros_like 는 주어진 배열과 동일한 형태와 타입을 가진 0 으로 이루어진 배열을 반환합니다.
+  #배치의 경사를 더하는 버퍼를 갱신할 것입니다
+  #모델은 k,v 쌍, 가중치, 층 등을 가집니다
 grad_buffer = { k : np.zeros_like(v) for k,v in model.iteritems() } 
-## rmsprop (gradient descent) memory used to update model
+## rmsprop (경사 하강법) 모델을 갱신하기 위해 사용된 메모리
 rmsprop_cache = { k : np.zeros_like(v) for k,v in model.iteritems() } 
 
-#activation function
+#활성 함수
 def sigmoid(x): 
-  return 1.0 / (1.0 + np.exp(-x)) # sigmoid "squashing" function to interval [0,1]
+  return 1.0 / (1.0 + np.exp(-x)) # sigmoid 로 함수의 구간을 [0,1] 로 압축시킵니다
 
-#takes a single game frame as input
-#preprocesses before feeding into model
+#하나의 게임 프레임을 입력으로 받습니다
+#모델에 적용하기 전 예비과정
 def prepro(I):
   """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
-  I = I[35:195] # crop
-  I = I[::2,::2,0] # downsample by factor of 2
-  I[I == 144] = 0 # erase background (background type 1)
-  I[I == 109] = 0 # erase background (background type 2)
-  I[I != 0] = 1 # everything else (paddles, ball) just set to 1
-  return I.astype(np.float).ravel() #flattens 
+  I = I[35:195] # 일부를 잘라냅니다
+  I = I[::2,::2,0] # 2 배 만큼 다운샘플합니다
+  I[I == 144] = 0 # 배경을 삭제합니다 (배경 타입 1)
+  I[I == 109] = 0 # 배경을 삭제합니다 (배경 타입 2)
+  I[I != 0] = 1 # 다른 모든 것들을 1 로 설정합니다
+  return I.astype(np.float).ravel() #평편화 
 
-#In practice the discount factor could be used to model the fact that the decision maker is uncertain 
-#about if in the next decision instant the world (e.g., environment / game / process ) is going to end.
-#If the decision maker is a robot, the discount factor could be the probability that the robot is switched 
-#off in the next time instant (the world ends in the previous terminology). That is the reason why the robot 
-#is short sighted and does not optimize the sum reward but the discounted sum reward.
+#실제로는 할인률이 결정권을 가진 자가
+#다음 결정으로 게임이 끝날지 확실치 않음을 보여줍니다
+#만약 결정권자가 로봇이라면, 할인률은 다음 턴에 로봇이 종료될 확률이 될 수 있습니다 
+#그래서 로봇이 멀리 내다보지 못하고,
+#할인된 보상의 합이 아닌 보상의 합을 최적화하지 못하는 것입니다
 
-#since we are otimizing for short term reward, not long term reward (like legend of zelda)
+#우리는 장기간이 아닌 단기간의 보상을 최적화합니다 (젤다의 전설 같이 말이죠)
 def discount_rewards(r):
   """ take 1D float array of rewards and compute discounted reward """
-  #initilize discount reward matrix as empty
+  #할인 보상 행렬을 빈 행렬로 초기화합니다
   discounted_r = np.zeros_like(r)
-  #to store reward sums
+  #보상의 합을 저장합니다
   running_add = 0
-  #for each reward
+  #각 보상마다 반복
   for t in reversed(xrange(0, r.size)):
-    #if reward at index t is nonzero, reset the sum, since this was a game boundary (pong specific!)
+    #t 에서의 보상이 0 이 아니면, 게임의 경계이기 때문에 합을 재설정합니다
     if r[t] != 0: running_add = 0 
-    #increment the sum 
+    #총합에 더해줍니다
     #https://github.com/hunkim/ReinforcementZeroToAll/issues/1
     running_add = running_add * gamma + r[t]
-    #earlier rewards given more value over time 
-    #assign the calculated sum to our discounted reward matrix
+    #더 많은 값이 주어질 때 이전의 보상
+    #계산한 총합을 할인 보상 행렬에 지정해줍니다
     discounted_r[t] = running_add
   return discounted_r
 
 
-#forward propagation via numpy woot!
+#numpy woot 를 이용한 순전파!
 def policy_forward(x):
-  #matrix multiply input by the first set of weights to get hidden state
-  #will be able to detect various game scenarios (e.g. the ball is in the top, and our paddle is in the middle)
+  #은닉 상태를 얻기 위한 첫 가중치를 입력으로 받는 행렬의 곱셈으로
+  #여러 게임 시나리오를 감지할 수 있습니다
   h = np.dot(model['W1'], x)
-  #apply an activation function to it
-  #f(x)=max(0,x) take max value, if less than 0, use 0
-  h[h<0] = 0 # ReLU nonlinearity
-  #repeat process once more
-  #will decide if in each case we should be going UP or DOWN.
+  #활성 함수를 적용해줍니다
+  #f(x)=max(0,x) 최대값을 취합니다. 만약 0 보다 작다면 0 을 사용합니다
+  h[h<0] = 0 # ReLU 비선형성
+  #과정을 한 번 더 반복합니다
+  #각 경우에 위 혹은 아래로 움직일지 결정합니다
   logp = np.dot(model['W2'], h)
-  #squash it with an activation (this time sigmoid to output probabilities)
+  #활성화를 통해 압축합니다 (이 경우에는 sigmoid 를 이용해 확률을 출력합니다)
   p = sigmoid(logp)
-  return p, h # return probability of taking action 2, and hidden state
+  return p, h # 2 번 동작의 확률과 은닉 상태를 반환합니다
 
 def policy_backward(eph, epdlogp):
   """ backward pass. (eph is array of intermediate hidden states) """
-  #recursively compute error derivatives for both layers, this is the chain rule
-  #epdlopgp modulates the gradient with advantage
-  #compute updated derivative with respect to weight 2. It's the parameter hidden states transpose * gradient w/ advantage (then flatten with ravel())
+  #두 층의 파생 오류를 재귀적으로 계산하는 것을 연쇄법칙이라 합니다
+  #epdlopgp 가 경사를 조절합니다
+  #가중치 2 의 갱신된 파생값을 계산합니다. 매개변소는 은닉 상태의 전치행렬 * 경사 입니다 (그리고 ravel() 로 평편화 해줍니다)
   dW2 = np.dot(eph.T, epdlogp).ravel()
-  #Compute derivative hidden. It's the outer product of gradient w/ advatange and weight matrix 2 of 2
+  #숨겨진 파생값을 계산합니다. 경사와 2x2 가중치 행렬의 외적입니다.
   dh = np.outer(epdlogp, model['W2'])
-  #apply activation
-  dh[eph <= 0] = 0 # backpro prelu
-  #compute derivative with respect to weight 1 using hidden states transpose and input observation
+  #활성화를 적용합니다
+  dh[eph <= 0] = 0 # 역전파
+  #은닉 상태의 전치 행렬과 입력 관찰을 이용해 1 가중치로 파생값 계산
   dW1 = np.dot(dh.T, epx)
-  #return both derivatives to update weights
+  #두 파생값을 반환해 가중치를 갱신합니다
   return {'W1':dW1, 'W2':dW2}
 
-#environment
+#환경
 env = gym.make("Pong-v0")
-#Each timestep, the agent chooses an action, and the environment returns an observation and a reward.
-#The process gets started by calling reset, which returns an initial observation
+#각 시간 단위마다 에이전트는 행동을 취하고, 관찰과 보상을 환경을 통해 전달 받습니다
+#초기 관찰을 반환하는 reset 을 불러낼 때 과정을 시작할 수 있습니다
 observation = env.reset()
-prev_x = None # used in computing the difference frame
-#observation, hidden state, gradient, reward
+prev_x = None # 차이를 계산하는데 사용됩니다
+#관찰, 은닉 상태, 경사, 보상
 xs,hs,dlogps,drs = [],[],[],[]
-#current reward
+#현재 보상
 running_reward = None
-#sum rewards
+#총 보상
 reward_sum = 0
-#where are we?
+#어디까지 왔을까요?
 episode_number = 0
 
-#begin training!
+#훈련을 시작합니다!
 while True:
 
-  # preprocess the observation, set input to network to be difference image
-  #Since we want our policy network to detect motion
-  #difference image = subtraction of current and last frame
+  #관찰을 시작하고 입력을 신경망에 설정해 다른 이미지가 되게 합니다
+  #정책 신경망이 행동을 감지해야 합니다
+  #격차 이미지 = 최후 프레임에 현재값은 뺀 것
   cur_x = prepro(observation)
   x = cur_x - prev_x if prev_x is not None else np.zeros(D)
   prev_x = cur_x
-  #so x is our image difference, feed it in!
+  #이것이 이미지 격차이고, 이것을 적용해줍니다
 
-  # forward the policy network and sample an action from the returned probability
+  # 정책 네트워크를 실행하고 반환된 확률을 이용해 행동을 취합니다
   aprob, h = policy_forward(x)
-  #this is the stochastic part 
-  #since not apart of the model, model is easily differentiable
-  #if it was apart of the model, we'd have to use a reparametrization trick (a la variational autoencoders. so badass)
-  action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
+  #확률론적인 부분입니다
+  #모델로부터 멀리 떨어져있지 않게 때문에, 모델을 구분할 수 있습니다
+  #만약 모델로부터 떨어져있다면, 다시 매개변수로 나타냅니다
+  action = 2 if np.random.uniform() < aprob else 3 # 주사위를 굴립니다!
 
-  # record various intermediates (needed later for backprop)
-  xs.append(x) # observation
-  hs.append(h) # hidden state
-  y = 1 if action == 2 else 0 # a "fake label"
-  dlogps.append(y - aprob) # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
+  # 여러 중간값을 기록합니다 (이후 역전파에 사용됩니다)
+  xs.append(x) # 관찰
+  hs.append(h) # 은닉 상태
+  y = 1 if action == 2 else 0 #"가짜 라벨"
+  dlogps.append(y - aprob) # 이루어져야 할 동작을 수행하는 것 (헷갈린다면 http://cs231n.github.io/neural-networks-2/#losses 를 참고)
 
-  # step the environment and get new measurements
+  # 환경의 단계와 새로운 값을 가져옵니다
   env.render()
   observation, reward, done, info = env.step(action)
   reward_sum += reward
 
-  drs.append(reward) # record reward (has to be done after we call step() to get reward for previous action)
+  drs.append(reward) # 보상을 기록합니다 (이전 동작에 대해 보상을 얻기 위해 step() 을 호출한 후에 실행해야 합니다)
 
-  if done: # an episode finished
+  if done: # 에피소드 종료
     episode_number += 1
 
-    # stack together all inputs, hidden states, action gradients, and rewards for this episode
-    #each episode is a few dozen games
-    epx = np.vstack(xs) #obsveration
-    eph = np.vstack(hs) #hidden
-    epdlogp = np.vstack(dlogps) #gradient
-    epr = np.vstack(drs) #reward
-    xs,hs,dlogps,drs = [],[],[],[] # reset array memory
+    # 이 에피소드의 모든 입력, 은닉 상태, 동작 경사, 그리고 보상을 쌓아놓습니다
+    #각 에피소드는 몇 십 번의 게임입니다
+    epx = np.vstack(xs) #관찰
+    eph = np.vstack(hs) #은닉
+    epdlogp = np.vstack(dlogps) #경사
+    epr = np.vstack(drs) #보상
+    xs,hs,dlogps,drs = [],[],[],[] # 배열 메모리를 재설정합니다
 
-    #the strength with which we encourage a sampled action is the weighted sum of all rewards afterwards, but later rewards are exponentially less important
-    # compute the discounted reward backwards through time
+    #샘플 동작의 강점은 모든 보상의 가중치에 따른 합입니다. 하지만 이후에는 보상이 훨씬 덜 중요하게 됩니다
+    # 역방향으로 할인 보상을 계산합니다
     discounted_epr = discount_rewards(epr)
-    # standardize the rewards to be unit normal (helps control the gradient estimator variance)
+    # 단위 정규화되도록 보상을 표준화합니다 (경사 추정 분산을 조절하는데 도움을 줍니다)
     discounted_epr -= np.mean(discounted_epr)
     discounted_epr /= np.std(discounted_epr)
 
-    #advatnage - quantity which describes how good the action is compared to the average of all the action.
-    epdlogp *= discounted_epr # modulate the gradient with advantage (PG magic happens right here.)
+    #우위(advantage) - 어떤 동작이 평균적인 동작에 비해 얼마나 좋은지 알려주는 지표.
+    epdlogp *= discounted_epr # 우위(advantage)를 가지고 경사를 조절합니다 (PG 의 마법이 이곳에서 나타납니다)
     grad = policy_backward(eph, epdlogp)
-    for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
+    for k in model: grad_buffer[k] += grad[k] # 배치의 경사를 누적시킵니다
 
-    # perform rmsprop parameter update every batch_size episodes
+    # 각 batch_size 마다 rmsprop 변수 갱신을 수행합니다
     #http://68.media.tumblr.com/2d50e380d8e943afdfd66554d70a84a1/tumblr_inline_o4gfjnL2xK1toi3ym_500.png
     if episode_number % batch_size == 0:
       for k,v in model.iteritems():
@@ -225,7 +222,7 @@ while True:
         model[k] += learning_rate * g / (np.sqrt(rmsprop_cache[k]) + 1e-5)
         grad_buffer[k] = np.zeros_like(v) # reset batch gradient buffer
 
-    # boring book-keeping
+    # 지루한 부기
     running_reward = reward_sum if running_reward is None else running_reward * 0.99 + reward_sum * 0.01
     print('resetting env. episode reward total was %f. running mean: %f' % (reward_sum, running_reward))
     if episode_number % 100 == 0: pickle.dump(model, open('save.p', 'wb'))
@@ -233,5 +230,5 @@ while True:
     observation = env.reset() # reset env
     prev_x = None
 
-  if reward != 0: # Pong has either +1 or -1 reward exactly when game ends.
+  if reward != 0: # 퐁은 게임이 끝날 때 +1 혹은 -1 의 보상이 있습니다.
     print ('ep %d: game finished, reward: %f' % (episode_number, reward)) + ('' if reward == -1 else ' !!!!!!!!')
